7fb91a78d108d88a7cc55e79769eb885eb82b353
Adir <adir.jscode@gmail.com>
2024-02-17 14:19:43 +0600

    Rider Edit,Delete Done

--- Courier-Service-V1/Courier-Service-V1/Controllers/HomeController.cs
+++ Courier-Service-V1/Courier-Service-V1/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Courier_Service_V1.Controllers
         {
            
            var riderCount = _context.Riders.Count();
-            ViewBag.RiderCount = riderCount;
+           ViewBag.RiderCount = riderCount;
            
 
         }
@@ -36,7 +36,13 @@ namespace Courier_Service_V1.Controllers
         
         public IActionResult Rider()
         {
-            return View();
+            var riders = _context.Riders.ToList();
+            if (riders == null)
+            {
+                return NotFound();
+            }
+            return View(riders);
+            
         }
 
         public IActionResult AddRider()
@@ -84,6 +90,88 @@ namespace Courier_Service_V1.Controllers
             }
             
         }
+
+
+        public IActionResult DeleteRider(int? id)
+        {
+            //use tempdata for alert then delete
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var rider = _context.Riders.Find(id);
+            if (rider == null)
+            {
+                return NotFound();
+            }
+            _context.Riders.Remove(rider);
+            _context.SaveChanges();
+            return RedirectToAction("Rider");
+        }
+
+        //edit rider
+        public IActionResult EditRider(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var rider = _context.Riders.Find(id);
+            if (rider == null)
+            {
+                return NotFound();
+            }
+            return View(rider);
+        }
+
+        [HttpPost]
+        public IActionResult EditRider(Rider rider, IFormFile? file)
+        {
+            if (rider == null)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                //handle image
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    //handle if prevoius image exist
+                    if (rider.ImageUrl != null)
+                    {
+                        string imagePath = Path.Combine(wwwRootPath, rider.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                    }
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string RiderPath = Path.Combine(wwwRootPath, @"Images\Rider");
+                    using (var FileSteam = new FileStream(Path.Combine(RiderPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(FileSteam);
+                    }
+
+                    rider.ImageUrl = @"\Images\Rider\" + fileName;
+                }
+                else
+                {
+                    rider.ImageUrl = "";
+                }
+
+                _context.Riders.Update(rider);
+                _context.SaveChanges();
+                return RedirectToAction("Rider");
+            }
+            else
+            {
+                return View(rider);
+            }
+        }
+
+
+
         public IActionResult Privacy()
         {
             return View();
